40e2c709fe632372e367b8722173e3588353ff2d
Jasper van Noordenburg <jaspervannoordenburg@hotmail.com>
2023-05-13 20:10:30 +0200

    Add hash to statename to mask full typename from storage

--- BlazorSignalRBackplaneTest/State/Storage/SessionStateStorage.cs
+++ BlazorSignalRBackplaneTest/State/Storage/SessionStateStorage.cs
@@ -1,6 +1,6 @@
-﻿using Blazored.LocalStorage;
+﻿using System.Security.Cryptography;
+using System.Text;
 
-using Fluxor;
 using Fluxor.Persist.Storage;
 
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
@@ -18,13 +18,20 @@ namespace BlazorSignalRBackplaneTest.State.Storage
 
         public async ValueTask<string> GetStateJsonAsync(string statename)
         {
-            var result = await SessionStorage.GetAsync<string>(statename);
+            var result = await SessionStorage.GetAsync<string>(GetHashedName(statename));
             return result.Value ?? string.Empty;
         }
 
         public async ValueTask StoreStateJsonAsync(string statename, string json)
         {
-            await SessionStorage.SetAsync(statename, json);
+            await SessionStorage.SetAsync(GetHashedName(statename), json);
+        }
+
+        /// Hashing is done purely to mask the state names, as they include the entire namespace
+        private static string GetHashedName(string name)
+        {
+            byte[] hashBytes = MD5.HashData(Encoding.UTF8.GetBytes(name));
+            return BitConverter.ToString(hashBytes).Replace("-", string.Empty);
         }
     }
 }
