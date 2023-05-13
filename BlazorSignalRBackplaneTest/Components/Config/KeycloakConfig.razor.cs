b455072d8edeaad5a2b21060a6ee57e1f1b20d93
Jasper van Noordenburg <jaspervannoordenburg@hotmail.com>
2023-05-13 22:56:25 +0200

    Trying with state that is being received

---
+++ BlazorSignalRBackplaneTest/Components/Config/KeycloakConfig.razor.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+using BlazorSignalRBackplaneTest.Components.Config.Models;
+
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorSignalRBackplaneTest.Components.Config
+{
+    public partial class KeycloakConfig
+    {
+        [Inject]
+        private HttpClient HttpClient { get; set; } = default!;
+
+        private OidcConfiguration? Configuration = default!;
+
+        protected override async Task OnParametersSetAsync()
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://keycloak.jaspervannoordenburg.nl/realms/amazingcluster/.well-known/openid-configuration");
+            using HttpResponseMessage response = await HttpClient.SendAsync(request);
+            Configuration = await JsonSerializer.DeserializeAsync<OidcConfiguration>(response.Content.ReadAsStream());
+        }
+    }
+}
---
+++ BlazorSignalRBackplaneTest/Components/Config/Models/OidcConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorSignalRBackplaneTest.Components.Config.Models
+{
+    public class OidcConfiguration
+    {
+        [JsonPropertyName("issuer")]
+        public string Issuer { get; set; } = default!;
+
+        [JsonPropertyName("authorization_endpoint")]
+        public string AuthorizationEndpoint { get; set; } = default!;
+
+        [JsonPropertyName("token_endpoint")]
+        public string TokenEndpoint { get; set; } = default!;
+
+        [JsonPropertyName("introspection_endpoint")]
+        public string IntrospectionEndpoint { get; set; } = default!;
+
+        [JsonPropertyName("userinfo_endpoint")]
+        public string UserinfoEndpoint { get; set; } = default!;
+    }
+}
